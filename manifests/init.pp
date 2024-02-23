781759e889d6dd4adebc80320470cca9027e4cef
schiznik <schiznik@gmail.com>
2024-02-23 17:01:02 +0000

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,7 +13,7 @@ class memcached (
   Optional[Stdlib::Absolutepath] $logfile                                                    = $memcached::params::logfile,
   Boolean $logstdout                                                                         = false,
   Boolean $syslog                                                                            = false,
-  Variant[Stdlib::Absolutepath, 'absent'] $pidfile                                           = '/var/run/memcached.pid',
+  Optional[Stdlib::Absolutepath] $pidfile                                                    = '/var/run/memcached.pid',
   Boolean $manage_firewall                                                                   = false,
   $max_memory                                                                                = '95%',
   Optional[Variant[Integer, String]] $max_item_size                                          = undef,
